8da670fa4a9918a8e2b1955f5bc512ac3cc0bb24
David Ewing <davidewing@neo.rr.com>
2020-08-23 07:06:28 -0400

    Add type annotations to models.

--- tcg/models.py
+++ tcg/models.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Tuple
 import random
@@ -14,19 +15,19 @@ class Card:
     cost: int
 
 
-def make_default_deck():
+def make_default_deck() -> Tuple:
     costs = (0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8)
     return tuple([Card(cost) for cost in costs])
 
 
 @dataclass(frozen=True)
 class Deck:
-    cards: Tuple[Card] = field(default_factory=make_default_deck)
+    cards: Tuple[Card, ...] = field(default_factory=make_default_deck)
 
-    def draw(self, count: int=1):
+    def draw(self, count: int=1) -> Tuple[Tuple[Card, ...], Deck]:
         return self.cards[:count], Deck(self.cards[count:])
 
-    def shuffle(self):
+    def shuffle(self) -> Deck:
         cards = list(self.cards)
         random.shuffle(cards)
         return Deck(tuple(cards))
